daaf5db00b95531f0388d8b15f8326b173bc5d0b
friscolie <friscolieyanto@gmail.com>
2022-08-01 09:30:35 +0700

    modifying major structures

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from "../category-item/category-item.component";
+import "./directory.styles.scss";
+
+const Directory = ({ Categories }) => {
+  return (
+    <div className="directory-container">
+      {Categories.map((category) => {
+        return <CategoryItem key={category.id} category={category} />;
+      })}
+    </div>
+  );
+};
+
+export default Directory;
